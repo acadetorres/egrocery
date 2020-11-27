54eef7eedbf5c234750e47ada9d010657cf097c3
marivicthor <adrian01000011@gmail.com>
2020-11-27 19:16:12 +0800

    Added the layout, resource, and overall theme for the e-grocery app.

--- app/src/main/java/com/acadetorress/e_grocery/MainActivity.java
+++ app/src/main/java/com/acadetorress/e_grocery/MainActivity.java
@@ -2,7 +2,10 @@ package com.acadetorress.e_grocery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView banner = findViewById(R.id.banner);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.videoplayback;
+        Uri videoUri = Uri.parse(videoPath);
+        banner.setVideoURI(videoUri);
+        banner.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setVolume(0f,0f);
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+            }
+        });
+
     }
 }
